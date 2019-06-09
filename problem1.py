cbb8916c364a6601b738d4842b1af86342903c5d
bhavyavj <39678373+bhavyavj@users.noreply.github.com>
2019-06-10 00:54:23 +0530

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,9 @@
+user_name = input("Enter your name:")
+user_age = input("Enter your age:")
+val = int(user_age)
+if val<95:
+        print("You will become {} years old after {} years.".format(95,95-val)
+elif val==95:
+        print("You are {} years old.".format(95))
+else :
+        print("You have become {} years old before {} years.".format(95,val-95)
