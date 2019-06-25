000cc063fe62ebda216fde239dc211d3af0da63b
bhavyavj <39678373+bhavyavj@users.noreply.github.com>
2019-06-25 22:47:28 +0530

    solution of problem2.py

--- problem2.py
+++ problem2.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python36
 
+#you need to install google library for this program
+#install via 'pip install google'
+
+import webbrowser
+from googlesearch import search
+file=open('url.txt','a+')
+data=input("Enter data to be searched \t")
+searched=search(data,stop=10)
+for url in searched:
+	file.write(url+"\n\n")
+	webbrowser.open(url)
