23cc3e4ef8bb43c81b11b94fc472a2033e6732be
bhavyavj <39678373+bhavyavj@users.noreply.github.com>
2019-06-25 23:16:22 +0530

    solution of qrcode

---
+++ problem12.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3
+
+
+import os
+import time
+import pyqrcode
+
+from pyqrcode import QRCode
+from googlesearch import search
+
+web= input("please enter topic: ")
+# print(search(web))
+url=[]
+u=0
+for i in search(web, stop=3):
+	print(i)
+	time.sleep(1)
+	url.append(i)
+
+print(url)
+
+for i in range(3):
+    qr= pyqrcode.create(url[u])
+    qr.svg("url"+str(i)+".png", scale=8)
+    u+=1
+    print(qr.terminal()) 
+	
+	
+os.system('mkdir /var/www/html/qr')
+	
+os.system('mv *.png /var/www/html/qr')
